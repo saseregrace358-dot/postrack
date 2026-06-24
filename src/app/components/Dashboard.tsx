e96a0ab5c40d8a6162121afbc8771a30148bb3dc
seun <saseregrace358@gmail.com>
2026-06-24 11:51:58 +0100

    Fix auth headers

--- src/app/components/Dashboard.tsx
+++ src/app/components/Dashboard.tsx
@@ -192,7 +192,18 @@ const busyDay =
   }, [weekSales]);
 
   return (
-    <div className="p-4 md:p-2 space-y-3">
+   <div className="
+  bg-white
+  dark:bg-slate-900
+  border
+  border-slate-200
+  dark:border-slate-800
+  shadow-sm
+  hover:shadow-md
+  transition-all
+  rounded-xl
+  p-4
+">
 
       {/* HEADER */}
       <div className="flex justify-between items-center">
