17824a2fe1a1f004e4539ffb15966195dcb8d93a
seun <saseregrace358@gmail.com>
2026-06-25 15:16:01 +0100

    add cors test route

--- src/api/employee.ts
+++ src/api/employee.ts
@@ -1,4 +1,23 @@
-import api from "axios";
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default api;
 
 export const getEmployees = () =>
   api.get("/employees");
