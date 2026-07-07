bac6e858ddb151afc9cabf10633e02028a4a5c70
seun <saseregrace358@gmail.com>
2026-07-07 18:58:59 +0100

    Add fastapi-mail

--- posbackend/app/services/paystack.py
+++ posbackend/app/services/paystack.py
@@ -8,19 +8,27 @@ headers = {
     "Content-Type": "application/json",
 }
 
+
 def initialize_payment(
     email: str,
     amount: float,
     reference: str,
     callback_url: str,
+    business_id: str,
+    plan: str,
 ):
     url = "https://api.paystack.co/transaction/initialize"
 
     payload = {
         "email": email,
-        "amount": int(amount * 100),
+        "amount": int(amount * 100),   # Kobo
         "reference": reference,
         "callback_url": callback_url,
+
+        "metadata": {
+            "business_id": business_id,
+            "plan": plan,
+        },
     }
 
     response = requests.post(
@@ -29,7 +37,11 @@ def initialize_payment(
         headers=headers,
     )
 
+    print("PAYSTACK RESPONSE:")
+    print(response.json())
+
     return response.json()
+
 
 def verify_payment(reference: str):
 
