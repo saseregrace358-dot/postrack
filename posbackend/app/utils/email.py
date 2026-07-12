db44da4f6bf66dae09cb8973afe8123b7022a053
seun <saseregrace358@gmail.com>
2026-07-12 16:11:23 +0100

    Add fastapi-mail

--- posbackend/app/utils/email.py
+++ posbackend/app/utils/email.py
@@ -7,7 +7,7 @@ print("MAIL_USERNAME:", os.getenv("MAIL_USERNAME"))
 print("MAIL_FROM:", os.getenv("MAIL_FROM"))
 print("MAIL_PASSWORD exists:", os.getenv("MAIL_PASSWORD") is not None)
 
-cconf = ConnectionConfig(
+conf = ConnectionConfig(
     MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
     MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
     MAIL_FROM=os.getenv("MAIL_FROM"),
