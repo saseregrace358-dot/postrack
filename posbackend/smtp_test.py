f5098c8984ae6a45b34e6bb078434e3ff62e8fd1
seun <saseregrace358@gmail.com>
2026-07-12 19:10:56 +0100

    Configure Brevo SMTP using environment variables

--- posbackend/smtp_test.py
+++ posbackend/smtp_test.py
@@ -1,13 +1,23 @@
 import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import os
+MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+MAIL_FROM = os.getenv("MAIL_FROM")
 
-try:
-    server = smtplib.SMTP_SSL("smtp.gmail.com", 465, timeout=10)
-    print("Connected!")
-    server.login(
-        "saseregrace358@gmail.com",
-        "isvskjfjigdfjuzg"
-    )
-    print("Login successful!")
-    server.quit()
-except Exception as e:
-    print(repr(e))
+TO_EMAIL = "email"
+
+msg = MIMEMultipart()
+msg["From"] = MAIL_FROM
+msg["To"] = TO_EMAIL
+msg["Subject"] = "Brevo SMTP Test"
+
+msg.attach(MIMEText("Hello! This email was sent successfully using Brevo SMTP.", "plain"))
+
+server = smtplib.SMTP_SSL("smtp-relay.brevo.com", 465)
+server.login(MAIL_USERNAME, MAIL_PASSWORD)
+server.sendmail(MAIL_FROM, TO_EMAIL, msg.as_string())
+server.quit()
+
+print("EMAIL SENT SUCCESSFULLY")
