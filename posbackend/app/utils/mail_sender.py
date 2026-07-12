a5e0b2cf65d2e062127dd23d32d79f00f0659961
seun <saseregrace358@gmail.com>
2026-07-12 15:37:39 +0100

    Add fastapi-mail

--- posbackend/app/utils/mail_sender.py
+++ posbackend/app/utils/mail_sender.py
@@ -1,11 +1,23 @@
-from fastapi_mail import FastMail, MessageSchema, MessageType
-from app.utils.email import conf
+import os
+import smtplib
+from email.message import EmailMessage
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+MAIL_FROM = os.getenv("MAIL_FROM")
+
 
 async def send_reset_email(email: str, code: str):
-    message = MessageSchema(
-        subject="Reset your BizTrack POS Password",
-        recipients=[email],
-        body=f"""
+    msg = EmailMessage()
+
+    msg["Subject"] = "Reset your BizTrack POS Password"
+    msg["From"] = MAIL_FROM
+    msg["To"] = email
+
+    msg.set_content(f"""
 Hello,
 
 We received a request to reset your password.
@@ -16,13 +28,16 @@ Your verification code is:
 
 This code will expire in 10 minutes.
 
+Enter this code in the BizTrack POS app to continue resetting your password.
+
+If you did not request this password reset, you can safely ignore this email.
+
 Regards,
 BizTrack POS Team
-""",
-        subtype=MessageType.plain,
-    )
+""")
 
-    fm = FastMail(conf)
-    await fm.send_message(message)
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+        smtp.login(MAIL_USERNAME, MAIL_PASSWORD)
+        smtp.send_message(msg)
 
     print("Email sent successfully!")
