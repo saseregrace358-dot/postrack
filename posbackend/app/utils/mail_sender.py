25b87363ee2d7bfd067e495b6bd751e4f7bb106f
seun <saseregrace358@gmail.com>
2026-07-22 14:09:17 +0100

    Remove missing fonts.css import

--- posbackend/app/utils/mail_sender.py
+++ posbackend/app/utils/mail_sender.py
@@ -1,4 +1,37 @@
+import smtplib
+
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 import os
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+print("MAIL_USERNAME:", os.getenv("MAIL_USERNAME"))
+
+print("MAIL_FROM:", os.getenv("MAIL_FROM"))
+MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+MAIL_FROM = os.getenv("MAIL_FROM")
+
+
+
+async def send_reset_email(email: str, code: str):
+    msg = MIMEMultipart()
+    msg["From"] = MAIL_FROM
+    msg["To"] = email
+    msg["Subject"] = "Reset your BizTrack POS Password"
+
+    msg.attach(
+        MIMEText(
+            f"Your verification code is: {code}",
+            "plain"
+        )
+    )
+
+    import os
 import smtplib
 from dotenv import load_dotenv
 from email.mime.text import MIMEText
